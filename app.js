4bceb8f6a8b7fbb523f84aebda10fc1898a875ad
Lucas <lac.lucas96@gmail.com>
2022-06-20 18:36:26 -0300

    feat: add aria-attributes to input and validates input with regex

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const inputContainer = document.querySelector(".input-container");
 const input = document.querySelector(".input-container__input");
-const span = document.querySelector("#error-message");
+const span = document.querySelector("#message");
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (!input.value) {
+  const email = input.value;
+  if (!validateEmail(email)) {
     input.setAttribute("aria-invalid", true);
     inputContainer.classList.add("input-container--error");
+    span.textContent = `Please provide a valid email  `;
+    span.style.visibility = "visible";
+  } else {
+    span.textContent = `Thank you for signing up!`;
     span.style.visibility = "visible";
   }
+  form.reset();
 });
+
+function validateEmail(email) {
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (!regex.test(email)) return false;
+  return true;
+}
